84b5692f8da416c62efd2ad72cbaaa992d61a3b2
Kartik Chavan <chavankartik2004@gmail.com>
2026-08-01 23:52:05 +0530

    feat: add web manager module

--- include/command.h
+++ include/command.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Command
 {
 public:
     static std::string GetCommand();
     static bool ProcessCommand(const std::string &command);
+
+private:
+    static std::vector<std::string> history;
 };
